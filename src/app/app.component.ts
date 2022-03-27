1c3b7704e9dbe62e912ade6d457a51cdf08db3f3
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-27 20:12:10 -0300

    Ejemplo 1 Directiva estructural ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular015-directivas';
+  title = 'Directivas en Angular';
+  saludo = true;
 }
