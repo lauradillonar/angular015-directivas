91fd239e1d9ef8bd6df816251d285704a8ff29a3
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-29 09:50:30 -0300

    Cambios en tiempo real al añadir otro objeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Directivas en Angular';
   cartas = [
-    {palo: 'corazones', valor: 'A'},
-    {palo: 'picas', valor: '3'},
-    {palo: 'diamantes', valor: 'K'},
-    {palo: 'trebol', valor: 'Q'}
+    {palo: 'corazones', valor: 'A', icono: '♥'},  //  &hearts;
+    {palo: 'picas', valor: '3', icono: '♠'},      //  &spades;
+    {palo: 'diamantes', valor: 'K', icono: '♦'},  //  &diams;
+    {palo: 'trebol', valor: 'Q', icono: '♣'}      //  &clubs;
   ];
+
+  palo: string = '';
+  valor: string = '';
+  icono: string = '';
+
+  saveItem():void{
+    this.cartas.push(
+      {
+        palo: this.palo,
+        valor: this.valor,
+        icono: this.icono
+      }
+    );
+    console.log('Objeto añadido al array de objetos.');
+  }
 }
