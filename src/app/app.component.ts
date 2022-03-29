140bde1a12f2adc4038ac09989a16cf2b1d359f5
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-29 12:41:29 -0300

    Eliminando un objeto de un array y usando ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Carta {
+  palo: string;
+  valor: string;
+  icono: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Directivas en Angular';
-  cartas = [
+  cartas: Carta[] = [
     {palo: 'corazones', valor: 'A', icono: '♥'},  //  &hearts;
     {palo: 'picas', valor: '3', icono: '♠'},      //  &spades;
     {palo: 'diamantes', valor: 'K', icono: '♦'},  //  &diams;
@@ -17,6 +24,16 @@ export class AppComponent {
   palo: string = '';
   valor: string = '';
   icono: string = '';
+
+
+  eliminar(carta: Carta):void{
+    const indice = this.cartas.indexOf(carta);
+
+    this.cartas.splice(indice, 1);
+    this.cartas.indexOf(carta);
+
+    console.log('Item: ' + indice + ' ha sido eliminado ' + carta);
+  }
 
   saveItem():void{
     this.cartas.push(
