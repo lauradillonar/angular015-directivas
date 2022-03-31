637ee4a8cc2ba5393c9ce025a05f7bb98cfd7d6e
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-31 19:35:47 -0300

    Ejemplo de ngSwitch mas ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 
-interface Carta {
-  palo: string;
-  valor: string;
+interface Icono {
   icono: string;
 }
 
@@ -14,35 +12,10 @@ interface Carta {
 
 export class AppComponent {
   title = 'Directivas en Angular';
-  cartas: Carta[] = [
-    {palo: 'corazones', valor: 'A', icono: '♥'},  //  &hearts;
-    {palo: 'picas', valor: '3', icono: '♠'},      //  &spades;
-    {palo: 'diamantes', valor: 'K', icono: '♦'},  //  &diams;
-    {palo: 'trebol', valor: 'Q', icono: '♣'}      //  &clubs;
+  cartas: Icono[] = [
+    {icono: '♥'},  //  &hearts;
+    {icono: '♠'},  //  &spades;
+    {icono: '♦'},  //  &diams;
+    {icono: '♣'}   //  &clubs;
   ];
-
-  palo: string = '';
-  valor: string = '';
-  icono: string = '';
-
-
-  eliminar(carta: Carta):void{
-    const indice = this.cartas.indexOf(carta);
-
-    this.cartas.splice(indice, 1);
-    this.cartas.indexOf(carta);
-
-    console.log('Item: ' + indice + ' ha sido eliminado ' + carta);
-  }
-
-  saveItem():void{
-    this.cartas.push(
-      {
-        palo: this.palo,
-        valor: this.valor,
-        icono: this.icono
-      }
-    );
-    console.log('Objeto añadido al array de objetos.');
-  }
 }
