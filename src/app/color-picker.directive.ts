a2bb8882996c0e13c4f22c26cf8a03d0edf9dfbd
lauradillonar <laura.dillon.ar@gmail.com>
2022-04-03 10:59:40 -0300

    Ejemplo 2 Custom Directives usando ElementRef y HostListener

--- src/app/color-picker.directive.ts
+++ src/app/color-picker.directive.ts
@@ -1,19 +1,23 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appColorPicker]'
 })
 export class ColorPickerDirective {
 
-  constructor() { 
+  constructor(private miElemento: ElementRef) { 
     console.log("Directiva creada correctamente");
   }
 
   @HostListener('dblclick') dobleclick():void{
+    this.miElemento.nativeElement.style.backgroundColor = 'red';
+    this.miElemento.nativeElement.style.color = 'black';
     console.log('%c' + 'Has hecho doble click', 'color:red');
   }
 
   @HostListener('click') oneClick():void{
+    this.miElemento.nativeElement.style.backgroundColor = 'blue';
+    this.miElemento.nativeElement.style.color = 'white';
     console.log('%c'+ 'Has hecho un click', 'color:blue');
   }
 }
